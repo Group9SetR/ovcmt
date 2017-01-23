f6109b03d1870434d08953e84ffc7326e66535cd
Vincent Lee <vincent.h.lee@outlook.com>
2017-01-23 15:05:29 -0800

    Added a line comment to test commit.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@ echo '<h1> Hello </h1>';
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Okanagan Valley College of Massage Therapy Scheduling</title>
-
+        <!--testing commit-->
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
