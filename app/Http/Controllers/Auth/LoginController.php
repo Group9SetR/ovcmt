89593c37d9335988b1d6fcfdc93ca50966cf44e9
Daniel Capacio <danielcapacio@gmail.com>
2017-02-11 20:38:19 -0800

    LoginController redirect routes, Kernel.php changes

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers\Auth;
 
-use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
+use DB;
+use Illuminate\Foundation\Auth\Access\AuthorizeResources;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -42,5 +45,57 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest', ['except' => 'logout']);
+    }
+
+    public function login(Request $req)
+    {
+        $email = $req->input('email');
+        $password = $req->input('password');
+
+        echo "email: $email <br>";
+        echo "password: $password <br>";
+
+        // $checkLogin = DB::table('users')->where(['email'=>$email,'password'=>$password])->get();
+
+        $checkEmail = DB::table('users')->where(['email' => $email])->get();
+
+        /*
+         * if email within database
+         *      if password is correct and matches email
+         *          if stud
+         *              redirect to stud page
+         *          if staff
+         *              redirect to staff page
+         *          if admin
+         *              redirect to admin page
+         *      else
+         *          display 'incorrect password' message
+         *          and redirect back to empty login form
+         * else
+         *      display 'email is not registered'
+         *      and redirect back to empty login form
+         */
+        if (count($checkEmail) > 0) {
+            $correctPassword = DB::table('users')->where('email', $email)->value('password');
+            $userType = DB::table('users')->where('email', $email)->value('usertype');
+            if ($password == $correctPassword) {
+                // check userType
+                if ($userType == 'student') {
+                    return redirect()->action('PagesController@studauth');
+                } else if ($userType == 'staff') {
+                    return redirect()->action('PagesController@staffauth');
+                } else if ($userType == 'admin') {
+                    return redirect()->action('PagesController@adminauth');
+                }
+            } else {
+                // incorrect password
+                echo "<h1 style='color: red;'>incorrect password</h1> <br>";
+                // return redirect()->action('LoginController@login');
+            }
+        } else {
+            // email is not registered
+            echo "<h1 style='color: red;'>email is not registered</h1> <br>";
+            // return redirect()->action('LoginController@login');
+        }
     }
 }
