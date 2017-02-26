0190e194763484850818ed7bb803d353fff20b79
Vincent Lee <vincent.h.lee@outlook.com>
2017-02-26 13:17:05 -0800

    Layout of instructor form. No data processing.

--- resources/views/pages/manageInstructor.blade.php
+++ resources/views/pages/manageInstructor.blade.php
@@ -36,11 +36,42 @@
         <div class="col-sm-9">
             <h4><small>Manage Instructors </small></h4>
             <hr>
-            <h2>make form</h2>
-            <form action="#somePage.php">
-                <button type="submit">Upload</button>
-                <button type="submit">Delete</button>
-            </form>
+            <h2>Add a New Instructor</h2>
+            <button href="#addInstructForm" class="btn btn-action" data-toggle="collapse">Add Instructor</button>
+            <div class="collapse" id="addInstructForm">
+                <form action="#somePage.php">
+                    First name: <input type="text" name="instructFname">
+                    <br>
+                    Last name: <input type="text" name="instructLname">
+                    <br>
+                    Email: <input type="text" name="instructEmail">
+                    <p>Check all time slots for which instructor is available:</p>
+                    <table>
+                    <tr>
+                        <th>Time</th><th>Mon</th><th>Tues</th><th>Wed</th><th>Thurs</th><th>Fri</th>
+                    </tr>
+                    <tr>
+                        <td>Morn</td>
+                        <td><input type="checkbox" name="instructAvail[]" value="mon_am"></td>
+                        <td><input type="checkbox" name="instructAvail[]" value="tues_am"></td>
+                        <td><input type="checkbox" name="instructAvail[]" value="wed_am"></td>
+                        <td><input type="checkbox" name="instructAvail[]" value="th_am"></td>
+                        <td><input type="checkbox" name="instructAvail[]" value="fri_am"></td>
+                    </tr>
+                    <tr>
+                        <td>Aft</td>
+                        <td><input type="checkbox" name="instructAvail[]" value="mon_pm"></td>
+                        <td><input type="checkbox" name="instructAvail[]" value="tues_pm"></td>
+                        <td><input type="checkbox" name="instructAvail[]" value="wed_pm"></td>
+                        <td><input type="checkbox" name="instructAvail[]" value="thurs_pm"></td>
+                        <td><input type="checkbox" name="instructAvail[]" value="fri_pm"></td>
+                    </tr>
+                    </table>
+                    <button type="submit" class="btn btn-default">Submit</button>
+                </form>
+            </div>
+            <h2>Display Instructors</h2>
+
         </div>
     </div>
 </div>
