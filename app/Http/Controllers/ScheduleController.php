670391c4cb4ae4461a57dc2905d779c82770f5b9
Matthew Li <li.matthew.m@gmail.com>
2017-03-14 16:41:04 -0700

    prelim data displays

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Course;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -15,16 +16,22 @@ class ScheduleController extends Controller
 
     public function generateCourses()
     {
-        $courseofferings = DB::table('course AS c')
+        $courseofferings = DB::table('courses AS c')
             ->join('course_offering AS co', 'c.crs_id', '=', 'co.crs_id')
             ->select('c.crs_id AS crs_id', 'c.sessions_days AS sessions_days', 'c.crs_type AS crs_type', 'c.term AS term', 'co.instruct_id AS instruct_id', 'co.ta_id AS ta_id')
             ->get();
 
         return $courseofferings;
     }
+    public function listCourses() {
+        $courseList = Course::all();
+        return $courseList;
+    }
 
     public function index()
     {
-        return;
+        $courseofferings = $this->generateCourses();
+        $courseList = $this->listCourses();
+        return view('pages.addschedule', compact('courseofferings','courseList'));
     }
 }
--- resources/views/pages/addschedule.blade.php
+++ resources/views/pages/addschedule.blade.php
@@ -25,14 +25,21 @@
             </ul><br>
         </div>
 
-        <div class="col-sm-9">
+        <div class="col-sm-6">
             <h4><small>Add schedule</small></h4>
             <hr>
             <h2>Display schedule</h2>
-            <form action="#somePage.php">
-                <button type="submit">Upload</button>
-                <button type="submit">Delete</button>
-            </form>
+            @foreach($courseofferings as $offerings)
+
+            @endforeach
+        </div>
+        <div class="col-sm-3">
+            @foreach($courseList as $course)
+                <div class="panel panel-default">
+                    <div class="panel-heading">{{$course->crs_id}}</div>
+                    <div class="panel-body">Sessions Days: {{$course->sessions_days}} && Type: {{$course->type}}</div>
+                </div>
+            @endforeach
         </div>
     </div>
 </div>
