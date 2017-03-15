75593d330bc9c9d7cc945aca3df0804087a6edc1
Matthew Li <li.matthew.m@gmail.com>
2017-03-15 14:53:41 -0700

    Finished queries for AssignController, removed old vagrant stuff.

--- app/Http/Controllers/AssignController.php
+++ app/Http/Controllers/AssignController.php
@@ -6,5 +6,35 @@ use Illuminate\Http\Request;
 
 class AssignController extends Controller
 {
-    
+    public function getCourses() {
+        $courses = DB::table('courses')
+            ->whereNotIn('crs_id', function($query)
+            {
+                $query->select(DB::raw(1))
+                    ->from('course_instructor')
+                    ->whereRaw('course_instructor.crs_id = course.crs_id');
+            })
+            ->get();
+        return $courses;
+    }
+
+    public function getInstructors() {
+        $instructors = Instructor::all();
+        return $instructors;
+    }
+
+    public function getCourseInstructors() {
+        $courseinstructor = DB::table('courses AS c')
+            ->join('course_instructor AS ci', 'c.crs_id', '=', 'ci.crs_id')
+            ->join('instructor AS i', 'ci.instruct_id', '=', 'i.instruct_id')
+            ->select('')
+            ->get();
+        return $courseinstructor;
+    }
+
+    public function index() {
+        $courses = $this->getCourses();
+        $instructors = $this->getInstructors();
+        return view('pages.addschedule', compact('courses','instructors', 'courseinstructor'));
+    }
 }
