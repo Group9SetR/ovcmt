4c0fde8bd43d543ce89b59878265f8e4dbf1dd4a
Matthew Li <li.matthew.m@gmail.com>
2017-03-14 17:00:13 -0700

    Drag and drog testing

--- resources/views/pages/addschedule.blade.php
+++ resources/views/pages/addschedule.blade.php
@@ -1,6 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
+<script>
+    function allowDrop(ev) {
+        ev.preventDefault();
+    }
+
+    function drag(ev) {
+        ev.dataTransfer.setData("text", ev.target.id);
+    }
+
+    function drop(ev) {
+        ev.preventDefault();
+        var data = ev.dataTransfer.getData("text");
+        ev.target.appendChild(document.getElementById(data));
+    }
+</script>
 <div class="container-fluid">
     <div class="row content">
         <div class="col-sm-3 sidenav">
@@ -29,13 +44,93 @@
             <h4><small>Add schedule</small></h4>
             <hr>
             <h2>Display schedule</h2>
-            @foreach($courseofferings as $offerings)
-
-            @endforeach
+            <table>
+                <table ondrop="drop(event)" ondragover="allowDrop(event)">
+                    <tr>
+                        <th>&nbsp;</th>
+                        <th>Rm P1</th>
+                        <th>Rm P2</th>
+                        <th>Rm A1</th>
+                        <th>Rm P2</th>
+                    </tr>
+                    <tr>
+                        <td>MON - AM</td>
+                        <div>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </div>
+                    </tr>
+                    <tr>
+                        <td>MON - PM</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td>TUE - AM</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td>TUE - PM</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td>WED - AM</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td>WED - PM</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td>THU - AM</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td>THU - PM</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td>FRI - AM</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td>FRI - PM</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                    </tr>
+                </table>
+            </table>
         </div>
         <div class="col-sm-3">
             @foreach($courseList as $course)
-                <div class="panel panel-default">
+                <div class="panel panel-default" draggable="true" ondragstart="drag(event)">
                     <div class="panel-heading">{{$course->crs_id}}</div>
                     <div class="panel-body">Sessions Days: {{$course->sessions_days}} && Type: {{$course->type}}</div>
                 </div>
