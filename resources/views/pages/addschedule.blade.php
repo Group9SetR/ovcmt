8f7cdd2763804304e0a97c51a9dd63710663f095
Hansol <leehansol1993@gmail.com>
2017-03-16 14:48:45 -0700

    Select Term

--- resources/views/pages/addschedule.blade.php
+++ resources/views/pages/addschedule.blade.php
@@ -9,13 +9,35 @@
 
 
 
-            <div class="col-sm-9">
-                <h4><small>Create</small></h4>
+            <div class="col-sm-6">
+                <h4><small>Select Term</small></h4>
                 <hr>
                 <h3>Select Term</h3>
 
-                <br>
-                <a href="{{ url('/dragDrop') }}" onClick="">NEXT</a>
+                <div class="input-group-btn select" id="select1">
+                    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                        <span class="selected">Term 1</span> <span class="caret"></span></button>
+                    <ul class="dropdown-menu option" role="menu">
+                        <li id="1"><a href="#">Term 1</a></li>
+                        <li id="2"><a href="#">Term 2</a></li>
+                        <li id="3"><a href="#">Term 3</a></li>
+                        <li id="4"><a href="#">Term 4</a></li>
+                    </ul>
+                </div>
+
+                <script>
+                    $('body').on('click','.option li',function(){
+                        var i = $(this).parents('.select').attr('id');
+                        var v = $(this).children().text();
+                        var o = $(this).attr('id');
+                        $('#'+i+' .selected').attr('id',o);
+                        $('#'+i+' .selected').text(v);
+                    });
+                </script>
+                <br><br><br><br><br>
+                <div class="btn pull-right">
+                <a href="{{ url('/dragDrop') }}" class="btn btn-primary" role="button">Next</a>
+                </div>
             </div>
 
 
