ef3bc71f92a3219ceb311f6f7ee34a0b5bc5f0c8
Matthew Li <li.matthew.m@gmail.com>
2017-03-15 18:44:52 -0700

    the minorist of edits to schedule controller

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -8,16 +8,10 @@ use Illuminate\Support\Facades\DB;
 
 class ScheduleController extends Controller
 {
-    public function pretty_dump($var) {
-        echo "<pre>";
-        echo var_dump($var);
-        echo "</pre>";
-    }
-
     public function generateCourses()
     {
         $courseofferings = DB::table('courses AS c')
-            ->join('course_offering AS co', 'c.crs_id', '=', 'co.crs_id')
+            ->join('course_offerings AS co', 'c.crs_id', '=', 'co.crs_id')
             ->select('c.crs_id AS crs_id', 'c.sessions_days AS sessions_days', 'c.crs_type AS crs_type', 'c.term AS term', 'co.instruct_id AS instruct_id', 'co.ta_id AS ta_id')
             ->get();
         return $courseofferings;
