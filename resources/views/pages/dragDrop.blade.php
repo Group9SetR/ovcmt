b568f8485712249326e42487100c5c2124e0f169
Vincent Lee <vincent.h.lee@outlook.com>
2017-03-18 21:38:35 -0700

    pull course time slots from rooms by days when they exist and place them on schedule, drag and copy course listings instead of drag and drop

--- resources/views/pages/dragDrop.blade.php
+++ resources/views/pages/dragDrop.blade.php
@@ -9,10 +9,22 @@
             ev.dataTransfer.setData("text", ev.target.id);
         }
 
-        function drop(ev) {
+        function drop(ev, el) {
             ev.preventDefault();
             var data = ev.dataTransfer.getData("text");
-            ev.target.appendChild(document.getElementById(data));
+            /*TODO Implement a deletion button on all drag_course_offering*/
+            /*TODO If a copydrop is deleted, increment the sessions counter*/
+            if(document.getElementById(data).classList.contains('drag_course_offering_listing')) {
+                var nodeCopy = document.getElementById(data).cloneNode(true);
+                nodeCopy.id = data+'-'; /* We cannot use the same ID */
+                nodeCopy.classList.remove('drag_course_offering_listing');
+                /*TODO This is a course listing so we need to decrement the sessions for each copydrop*/
+                var theId = '#'+data;
+                /*TODO Also need to remove the sessions from the copied element.*/
+                ev.target.appendChild(nodeCopy);
+            } else {
+                el.appendChild(document.getElementById(data));
+            }
         }
 
         $(document).ready(function() {
@@ -49,7 +61,7 @@
                 <div class="row">
                     <div class="col-md-10">
                         {!! Form::open(['url' => 'dragDrop']) !!}
-                        <table ondrop="drop(event)" class='table table-bordered' id='drag_schedule_table'>
+                        <table class='table table-bordered' id='drag_schedule_table'>
                             <thead>
                             <tr>
                                 <th class='drag_schedule_row_head'>Room</th>
@@ -63,36 +75,36 @@
                             <tbody>
                             <tr class="drag_schedule_row">
                                 <th class='drag_schedule_row_head'>M1-AM</th>
-                                <td ondragover="allowDrop(event)" name="M1-am[]"></td>
-                                <td ondragover="allowDrop(event)" name="M1-am[]"></td>
-                                <td ondragover="allowDrop(event)" name="M1-am[]"></td>
-                                <td ondragover="allowDrop(event)" name="M1-am[]"></td>
-                                <td ondragover="allowDrop(event)" name="M1-am[]"></td>
+                                <td ondrop="drop(event, this)" ondragover="allowDrop(event)" name="M1-am[]"></td>
+                                <td ondrop="drop(event, this)" ondragover="allowDrop(event)" name="M1-am[]"></td>
+                                <td ondrop="drop(event, this)"  ondragover="allowDrop(event)" name="M1-am[]"></td>
+                                <td ondrop="drop(event, this)"  ondragover="allowDrop(event)" name="M1-am[]"></td>
+                                <td ondrop="drop(event, this)"  ondragover="allowDrop(event)" name="M1-am[]"></td>
                             </tr>
                             <tr class="drag_schedule_row">
                                 <th class='drag_schedule_row_head'>A1-AM</th>
-                                <td ondragover="allowDrop(event)" name="A1-am[]"></td>
-                                <td ondragover="allowDrop(event)" name="A1-am[]"></td>
-                                <td ondragover="allowDrop(event)" name="A1-am[]"></td>
-                                <td ondragover="allowDrop(event)" name="A1-am[]"></td>
-                                <td ondragover="allowDrop(event)" name="A1-am[]"></td>
+                                <td ondrop="drop(event, this)" ondragover="allowDrop(event)" id="A1-AM-0" name="A1-am[]"></td>
+                                <td ondrop="drop(event, this)" ondragover="allowDrop(event)" id="A1-AM-1" name="A1-am[]"></td>
+                                <td ondrop="drop(event, this)" ondragover="allowDrop(event)" id="A1-AM-2" name="A1-am[]"></td>
+                                <td ondrop="drop(event, this)" ondragover="allowDrop(event)" id="A1-AM-3" name="A1-am[]"></td>
+                                <td ondrop="drop(event, this)" ondragover="allowDrop(event)" id="A1-AM-4" name="A1-am[]"></td>
                             </tr>
                             <tr class="drag_schedule_row">
                                 <th class='drag_schedule_row_head'>P1-AM</th>
-                                <td ondragover="allowDrop(event)" name="P1-am[]"></td>
-                                <td ondragover="allowDrop(event)" name="P1-am[]"></td>
-                                <td ondragover="allowDrop(event)" name="P1-am[]"></td>
-                                <td ondragover="allowDrop(event)" name="P1-am[]"></td>
-                                <td ondragover="allowDrop(event)" name="P1-am[]"></td>
+                                <td ondrop="drop(event, this)" ondragover="allowDrop(event)" name="P1-am[]"></td>
+                                <td ondrop="drop(event, this)" ondragover="allowDrop(event)" name="P1-am[]"></td>
+                                <td ondrop="drop(event, this)" ondragover="allowDrop(event)" name="P1-am[]"></td>
+                                <td ondrop="drop(event, this)" ondragover="allowDrop(event)" name="P1-am[]"></td>
+                                <td ondrop="drop(event, this)" ondragover="allowDrop(event)" name="P1-am[]"></td>
                             </tr>
                             <tr class="drag_schedule_row">
                                 <th class='drag_schedule_row_head'>P2-AM</th>
 
-                                <td ondragover="allowDrop(event)" name="P2-am[]"></td>
-                                <td ondragover="allowDrop(event)" name="P2-am[]"></td>
-                                <td ondragover="allowDrop(event)" name="P2-am[]"></td>
-                                <td ondragover="allowDrop(event)" name="P2-am[]"></td>
-                                <td ondragover="allowDrop(event)" name="P2-am[]"></td>
+                                <td ondrop="drop(event, this)" ondragover="allowDrop(event)" name="P2-am[]"></td>
+                                <td ondrop="drop(event, this)" ondragover="allowDrop(event)" name="P2-am[]"></td>
+                                <td ondrop="drop(event, this)" ondragover="allowDrop(event)" name="P2-am[]"></td>
+                                <td ondrop="drop(event, this)" ondragover="allowDrop(event)" name="P2-am[]"></td>
+                                <td ondrop="drop(event, this)" ondragover="allowDrop(event)" name="P2-am[]"></td>
                             </tr>
                             <tr > <!--Spacing row-->
                                 <th></th>
@@ -104,47 +116,48 @@
                             </tr>
                             <tr class="drag_schedule_row">
                                 <th class='drag_schedule_row_head'>M1-PM</th>
-                                <td ondragover="allowDrop(event)" name="M1-pm[]"></td>
-                                <td ondragover="allowDrop(event)" name="M1-pm[]"></td>
-                                <td ondragover="allowDrop(event)" name="M1-pm[]"></td>
-                                <td ondragover="allowDrop(event)" name="M1-pm[]"></td>
-                                <td ondragover="allowDrop(event)" name="M1-pm[]"></td>
+                                <td ondrop="drop(event, this)" ondragover="allowDrop(event)" name="M1-pm[]"></td>
+                                <td ondrop="drop(event, this)" ondragover="allowDrop(event)" name="M1-pm[]"></td>
+                                <td ondrop="drop(event, this)" ondragover="allowDrop(event)" name="M1-pm[]"></td>
+                                <td ondrop="drop(event, this)" ondragover="allowDrop(event)" name="M1-pm[]"></td>
+                                <td ondrop="drop(event, this)" ondragover="allowDrop(event)" name="M1-pm[]"></td>
                             </tr>
                             <tr class="drag_schedule_row">
                                 <th class='drag_schedule_row_head'>A1-PM</th>
-                                <td ondragover="allowDrop(event)" name="A1-pm[]"></td>
-                                <td ondragover="allowDrop(event)" name="A1-pm[]"></td>
-                                <td ondragover="allowDrop(event)" name="A1-pm[]"></td>
-                                <td ondragover="allowDrop(event)" name="A1-pm[]"></td>
-                                <td ondragover="allowDrop(event)" name="A1-pm[]"></td>
+                                <td ondrop="drop(event, this)" ondragover="allowDrop(event)" name="A1-pm[]"></td>
+                                <td ondrop="drop(event, this)" ondragover="allowDrop(event)" name="A1-pm[]"></td>
+                                <td ondrop="drop(event, this)" ondragover="allowDrop(event)" name="A1-pm[]"></td>
+                                <td ondrop="drop(event, this)" ondragover="allowDrop(event)" name="A1-pm[]"></td>
+                                <td ondrop="drop(event, this)" ondragover="allowDrop(event)" name="A1-pm[]"></td>
                             </tr>
                             <tr class="drag_schedule_row">
                                 <th class='drag_schedule_row_head'>P1-PM</th>
-                                <td ondragover="allowDrop(event)" name="P1-pm[]"></td>
-                                <td ondragover="allowDrop(event)" name="P1-pm[]"></td>
-                                <td ondragover="allowDrop(event)" name="P1-pm[]"></td>
-                                <td ondragover="allowDrop(event)" name="P1-pm[]"></td>
-                                <td ondragover="allowDrop(event)" name="P1-pm[]"></td>
+                                <td ondrop="drop(event, this)" ondragover="allowDrop(event)" name="P1-pm[]"></td>
+                                <td ondrop="drop(event, this)" ondragover="allowDrop(event)" name="P1-pm[]"></td>
+                                <td ondrop="drop(event, this)" ondragover="allowDrop(event)" name="P1-pm[]"></td>
+                                <td ondrop="drop(event, this)" ondragover="allowDrop(event)" name="P1-pm[]"></td>
+                                <td ondrop="drop(event, this)" ondragover="allowDrop(event)" name="P1-pm[]"></td>
                             </tr>
                             <tr class="drag_schedule_row">
                                 <th class='drag_schedule_row_head'>P2-PM</th>
-                                <td ondragover="allowDrop(event)" name="P2-pm[]"></td>
-                                <td ondragover="allowDrop(event)" name="P2-pm[]"></td>
-                                <td ondragover="allowDrop(event)" name="P2-pm[]"></td>
-                                <td ondragover="allowDrop(event)" name="P2-pm[]"></td>
-                                <td ondragover="allowDrop(event)" name="P2-pm[]"></td>
+                                <td ondrop="drop(event, this)" ondragover="allowDrop(event)" name="P2-pm[]"></td>
+                                <td ondrop="drop(event, this)" ondragover="allowDrop(event)" name="P2-pm[]"></td>
+                                <td ondrop="drop(event, this)" ondragover="allowDrop(event)" name="P2-pm[]"></td>
+                                <td ondrop="drop(event, this)" ondragover="allowDrop(event)" name="P2-pm[]"></td>
+                                <td ondrop="drop(event, this)" ondragover="allowDrop(event)" name="P2-pm[]"></td>
                             </tr>
                             </tbody>
                         </table>
                         <div class="form-group">
                         {!! Form::submit('Save',['class'=> 'btn btn-primary ']) !!}
-                        <!-- {!! Form::submit('Clear',['class'=> 'btn btn-primary']) !!} -->
+                        {!! Form::submit('Clear',['class'=> 'btn btn-primary']) !!}
                             <a href="dragDrop"><button class='btn btn-primary'>Clear</button></a>
                         </div>
                         {{ Form::close() }}
                         @foreach ($amRoomsByWeek as $timeslot)
                             <script>
                                 var dayOfWeek ='<?php echo $timeslot->cdayOfWeek;?>';
+                                dayOfWeek--;
                                 dayOfWeek--;
                                 var room_id ='<?php echo $timeslot->room_id;?>';
                                 var crn='<?php echo $timeslot->am_crn;?>';
@@ -154,28 +167,33 @@
                                 coursePanel.setAttribute('id', 'slid');
                                 coursePanel.setAttribute('draggable', 'true');
                                 coursePanel.setAttribute('ondragstart','drag(event)');
+                                coursePanel.setAttribute('ondrop','return false;');
+                                coursePanel.setAttribute('ondragover', 'return false;');
                                 var coursePanelHeading=document.createElement('DIV');
+                                coursePanelHeading.className='panel-heading';
                                 coursePanelHeading.append(document.createElement('P').appendChild(document.createTextNode(dayOfWeek+sb)));
                                 console.log(crn);
                                 coursePanel.append(coursePanelHeading);
                                 var coursePanelBody = document.createElement('DIV');
                                 coursePanelBody.className=['panel-body drag_course_offering_panel'];
-                                coursePanelBody.append(document.createElement('P').appendChild(document.createTextNode('sessions')));
-                                coursePanelBody.append(document.createElement('P').appendChild(document.createTextNode('term')));
-                                coursePanelBody.append(document.createElement('P').appendChild(document.createTextNode('type')));
+                                coursePanelBody.append(document.createElement('P').appendChild(document.createTextNode('Sessions:\n')));
+                                coursePanelBody.append(document.createElement('P').appendChild(document.createTextNode('Term:\n')));
+                                coursePanelBody.append(document.createElement('P').appendChild(document.createTextNode('Type:\n')));
                                 coursePanel.append(coursePanelBody);
                                 document.getElementsByName(sb)[dayOfWeek].append(coursePanel);
                             </script>
                         @endforeach
                     </div>
-                    <div class="col-sm-2 drag_course_offering_list" ondragover="allowDrop(event)" ondrop="drop(event)">
+                    <div class="col-sm-2 drag_course_offering_list" ondragover="allowDrop(event)" ondrop="drop(event, this)">
                         @foreach($courseofferings as $course)
-                            <div class="panel panel-default drag_course_offering" id="slid" draggable="true" ondragstart="drag(event)">
+                            <div class="panel panel-default drag_course_offering drag_course_offering_listing" id="slid" draggable="true"
+                                 ondragstart="drag(event)" ondrop="return false;" ondragover="return false;">
                                 <div class="panel-heading">
                                     {{$course->course_id}} <b>CRN:</b>{{$course->crn}}
                                 </div>
                                 <div class="panel-body drag_course_offering_panel">
-                                    <b>Sessions:</b> {{$course->sessions_days}}<br>
+                                    <b class='drag_course_offering_listing_sessions'>Sessions:</b>
+                                        <span class='course_offering_listing_sessions_days'>{{$course->sessions_days}}</span><br>
                                     <b>Term: </b> {{$course->term_no}} <br>
                                     <b>Type:</b> {{$course->course_type}}
                                 </div>
