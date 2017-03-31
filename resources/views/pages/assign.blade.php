1e15e1d741e4fbf3d909850ab10d27bdc067b5c9
Daniel Capacio <danielcapacio@gmail.com>
2017-03-31 14:19:33 -0700

    Form-control in assign, we got 'em folks

--- resources/views/pages/assign.blade.php
+++ resources/views/pages/assign.blade.php
@@ -12,23 +12,17 @@
             <div class="col-sm-10">
                 <h4><small>Select a Term</small></h4>
                 <hr>
-                <div class="container">
-                    <div class="row" id="term_selector">
-                        {!! Form::open(['url' => '', 'class' => 'form-inline', 'id' => 'select_term']) !!}
-                        <div class="col-sm-5" id="assign_select_from">
-                            <select name="selected_term_id" id="selected_term_id">
-                                @foreach ($terms as $term)
-                                    <option value={{$term->term_id}}>Term Id: {{$term->term_id}}, Term Number:{{$term->term_no}},
-                                        Intake Number:{{$term->intake_id}}, Start Date:{{$term->term_start_date}} </option>
-                                @endforeach
-                            </select>
-                        </div>
-                        <div class="col-sm-2">
-                            {!! Form::submit('Choose Term',['class'=> 'btn btn-primary form-inline']) !!}
-                        </div>
-                        {!! Form::close() !!}
-                    </div>
+                {!! Form::open(['url' => '', 'class' => 'form-inline', 'id' => 'select_term']) !!}
+                <div class="form-inline">
+                    <select name="selected_term_id" id="selected_term_id" class="form-control">
+                        @foreach ($terms as $term)
+                            <option value={{$term->term_id}}>Term Id: {{$term->term_id}}, Term Number:{{$term->term_no}},
+                                Intake Number:{{$term->intake_id}}, Start Date:{{$term->term_start_date}} </option>
+                        @endforeach
+                    </select>
+                    {!! Form::submit('Choose Term',['class'=> 'btn btn-primary form-inline']) !!}
                 </div>
+                {!! Form::close() !!}
 
                 <div class="modal fade" id="assignCoursesToInstructor" tabindex="-1" role="dialog">
                     <div class="modal-dialog">
