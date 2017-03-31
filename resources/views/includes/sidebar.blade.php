9d8e3747e96deae26da2824c8188e45c35895354
Matthew Li <li.matthew.m@gmail.com>
2017-03-31 15:37:08 -0700

    adjust sidebar just so I can remember what order to do things

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -31,15 +31,15 @@
 </ul><br>
 
 <ul class="nav nav-pills nav-stacked sidebar">
-    <li><a href="{{ url('/manageTerm') }}" onClick="">Manage Term</a></li>
-</ul><br>
-
-<ul class="nav nav-pills nav-stacked sidebar">
     <li><a href="{{ url('/manageInstructor') }}" onClick="">Manage Instructor</a></li>
 </ul><br>
 
 <ul class="nav nav-pills nav-stacked sidebar">
     <li><a href="{{ url('/manageCourse') }}" onClick="">Manage Course</a></li>
+</ul><br>
+
+<ul class="nav nav-pills nav-stacked sidebar">
+    <li><a href="{{ url('/manageTerm') }}" onClick="">Manage Term</a></li>
 </ul><br>
 
 <ul class="nav nav-pills nav-stacked sidebar">
