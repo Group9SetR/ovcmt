f6ad92e94512317f833840ec6e840adbc4972f84
Vincent Lee <vincent.h.lee@outlook.com>
2017-04-07 18:12:27 -0700

    clean up code and visuals

--- resources/views/pages/manageInstructor.blade.php
+++ resources/views/pages/manageInstructor.blade.php
@@ -190,8 +190,6 @@
                                     success: function (data) {
                                         $('#courseListing').empty();
                                         for (let i = 0; i < data['courses'].length; i++) {
-                                            console.log(data['courses'][i]['instructor_id']);
-                                            console.log(data['courses'][i]['course_id']);
                                             var panel = "<div class='panel panel-default'><div class='panel-heading'><div class='row'><div class='col-sm-4 text-left'>" + data['courses'][i]['course_id']
                                                 + "</div><div class='col-md-8 text-right'>" +
                                                 '{{Form::open(["url" => "deleteCourseInstructor"])}}' +
@@ -324,8 +322,6 @@
         $(document).on('click', '.open-DeleteInstructorDialog', function() {
             document.getElementById('deleteInstructorForm').reset();
             var instructor_id = $(this).parent().siblings(":first").text();
-            console.log(instructor_id);
-
             $('.modal-body #modal_instructorid_delete').attr('value', instructor_id);
         });
     </script>
