eb3ae6f01d089fc6b61a0ef4017fa193f3ac59ca
Matthew Li <li.matthew.m@gmail.com>
2017-04-08 18:34:34 -0700

    spelling fixes

--- resources/views/pages/scheduleinstructor.blade.php
+++ resources/views/pages/scheduleinstructor.blade.php
@@ -16,9 +16,9 @@
             <div class="col-sm-10">
                 <h4><small>Display Schedule</small></h4>
                 <hr>
-                <!-- TODO Display only schedules by term-->
+                <!-- TODO Display only schedules by term--S
                 <!-- TODO Date picker -->
-                <button class="btn btn-mg btn-default"> <a href="javascript:pdfToHTML()">Download schedual</a></button>
+                <button class="btn btn-mg btn-default"> <a href="javascript:pdfToHTML()">Download Schedule</a></button>
                 <div id ="pdf2htmldiv">
                 <div class="row">
                     <div class="col-md-6">
